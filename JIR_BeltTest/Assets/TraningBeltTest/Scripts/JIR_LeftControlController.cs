377e456d8da97689c4b199d38689568398f53af7
Inryeong <gseeker621@gmail.com>
2020-10-05 04:23:47 +0900

    Final app commit

---
+++ JIR_BeltTest/Assets/TraningBeltTest/Scripts/JIR_LeftControlController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JIR_LeftControlController : MonoBehaviour
+{
+
+    public SphereCollider ir_r_cc;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "StartBtn")
+        {
+            gameObject.GetComponent<SphereCollider>().radius = 0.8f;
+            ir_r_cc.GetComponent<SphereCollider>().radius = 0.8f;
+        }
+    }
+
+}
---
+++ JIR_BeltTest/Assets/TraningBeltTest/Scripts/JIR_RightControlController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class JIR_RightControlController : MonoBehaviour
+{
+    //1. 버튼 충돌 여부에 따라서 컨트롤러의 콜라이더 컴포넌트를 껐다 켰다 하고싶다
+
+    public SphereCollider ir_l_cc;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "StartBtn")
+        {
+            gameObject.GetComponent<SphereCollider>().radius = 0.8f;
+            ir_l_cc.GetComponent<SphereCollider>().radius = 0.8f;
+        }
+    }
+}
